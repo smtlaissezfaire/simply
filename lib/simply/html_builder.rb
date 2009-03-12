f8ac6e7c1f69a4d3a1b5dcfcfc370da3431d79ea
Scott Taylor <scott@railsnewbie.com>
2009-03-12 02:01:44 -0400

    Engage in method hiding by double underscoring private methods

--- lib/simply/html_builder.rb
+++ lib/simply/html_builder.rb
@@ -11,7 +11,7 @@ module Simply
     SELF_CLOSING_TAGS.each do |tag|
       class_eval <<-HERE, __FILE__, __LINE__
         def #{tag}(attributes={})
-          self_closing_tag(:#{tag}, attributes)
+          __self_closing_tag(:#{tag}, attributes)
         end
       HERE
     end
@@ -26,7 +26,7 @@ module Simply
             options = args.first || { }
           end
 
-          tag(:#{tag}, options, &block)
+          __tag(:#{tag}, options, &block)
         end
       HERE
     end
@@ -55,33 +55,33 @@ module Simply
 
   private
 
-    def tag(tag_name, attributes={ }, &block)
-      text opening_tag(tag_name, attributes)
+    def __tag(tag_name, attributes={ }, &block)
+      text __opening_tag(tag_name, attributes)
       instance_eval(&block)
-      text closing_tag(tag_name)
+      text __closing_tag(tag_name)
     end
 
-    def opening_tag(tag_name, attributes={ })
+    def __opening_tag(tag_name, attributes={ })
       if attributes.any?
-        "<#{tag_name} #{expand_attributes(attributes)}>"
+        "<#{tag_name} #{__expand_attributes(attributes)}>"
       else
         "<#{tag_name}>"
       end
     end
 
-    def closing_tag(tag_name)
+    def __closing_tag(tag_name)
       "</#{tag_name}>"
     end
 
-    def self_closing_tag(tag_name, attributes={ })
+    def __self_closing_tag(tag_name, attributes={ })
       if attributes.any?
-        text "<#{tag_name} #{expand_attributes(attributes)} />"
+        text "<#{tag_name} #{__expand_attributes(attributes)} />"
       else
         text "<#{tag_name} />"
       end
     end
 
-    def expand_attributes(attributes)
+    def __expand_attributes(attributes)
       attributes.map { |key, value| "#{key}=\"#{value}\""}.join(" ")
     end
   end
