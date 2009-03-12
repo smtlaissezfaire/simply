5c4d7dc39edd42bc36e4814cb684b4eac26736fc
Scott Taylor <scott@railsnewbie.com>
2009-03-12 02:34:47 -0400

    Add documentation for locals

--- lib/simply/html_builder.rb
+++ lib/simply/html_builder.rb
@@ -54,6 +54,17 @@ module Simply
     #
     ####################
 
+    # Pass in a hash of local variables
+    # which will be available to the template:
+    #
+    #    @builder.locals = { :foo => 1, :bar => 2, :baz => 3}
+    #    @builder.ul do
+    #      li foo
+    #      li bar
+    #      li baz
+    #    end
+    #
+    #    @builder.to_s #=> "<ul><li>1</li><li>2</li><li>3</li></ul>"
     def locals=(hash={ })
       def metaclass(&block)
         meta = class << self; self; end
