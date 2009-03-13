bfa016b4c6b9013b632cb9845170a6894c79f1b9
Scott Taylor <scott@railsnewbie.com>
2009-03-13 05:07:28 -0400

    Move around requires for clarity (to show dependencies)

--- lib/simply.rb
+++ lib/simply.rb
@@ -3,9 +3,9 @@ module Simply
 
   require "rubygems"
   require "builder/xchar"
+  require "#{dir}/indentation"
   require "#{dir}/html_tags"
   require "#{dir}/html_builder"
-  require "#{dir}/indentation"
 end
 
 def Simply(options={}, &block)
