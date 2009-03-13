cebe074856f531fef221c91217c777ce9db3287b
Scott Taylor <scott@railsnewbie.com>
2009-03-13 05:32:21 -0400

    Update version in specs

--- spec/simply/version_spec.rb
+++ spec/simply/version_spec.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + "/../spec_helper"
 
 describe Simply, "'s Version" do
-  it "should be at 0.1.0" do
-    Simply::VERSION.should == "0.1.0"
+  it "should be at 0.1.1" do
+    Simply::VERSION.should == "0.1.1"
   end
 end
