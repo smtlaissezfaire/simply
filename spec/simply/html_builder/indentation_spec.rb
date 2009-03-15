cca229823a45615bf05cde825c54b562166b814d
Scott Taylor <scott@railsnewbie.com>
2009-03-15 00:45:44 -0400

    Make indentation the default

--- spec/simply/html_builder/indentation_spec.rb
+++ spec/simply/html_builder/indentation_spec.rb
@@ -3,22 +3,22 @@ require File.dirname(__FILE__) + "/../../spec_helper"
 module Simply
   describe HTMLBuilder, "with indentation" do
     before(:each) do
-      @builder = HTMLBuilder.new(:indent => true)
+      @builder = HTMLBuilder.new(:indented => true)
     end
     
-    it "should be indented when passed :indent => true" do
-      builder = HTMLBuilder.new(:indent => true)
+    it "should be indented when passed :indented => true" do
+      builder = HTMLBuilder.new(:indented => true)
       builder.should be_indented
     end
     
-    it "should not be indented when passed :indent => false" do
-      builder = HTMLBuilder.new(:indent => false)
+    it "should not be indented when passed :indented => false" do
+      builder = HTMLBuilder.new(:indented => false)
       builder.should_not be_indented
     end
     
-    it "should not be indented by default" do
+    it "should be indented by default" do
       builder = HTMLBuilder.new
-      builder.should_not be_indented
+      builder.should be_indented
     end
     
     it "should indent html when indented = true, and given a block structure" do
