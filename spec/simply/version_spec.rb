49646dfea8b2d1fbf03c759f295749c1495bbe91
Scott Taylor <scott@railsnewbie.com>
2009-05-15 15:01:42 -0400

    Version bump to 0.1.2

--- spec/simply/version_spec.rb
+++ spec/simply/version_spec.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + "/../spec_helper"
 
 describe Simply, "'s Version" do
-  it "should be at 0.1.1" do
-    Simply::VERSION.should == "0.1.1"
+  it "should be at 0.1.2" do
+    Simply::VERSION.should == "0.1.2"
   end
 end
